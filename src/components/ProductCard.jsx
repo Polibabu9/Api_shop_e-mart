8dd1f48f7c7cc63f1609443af6bea7c80d9a93f4
Polibabu9 <addalapoli1@gmail.com>
2025-07-02 18:56:27 +0530

    some changes

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './ProductCard.css';
-
 function ProductCard(props) {
   return (
     <>
-    <div className='card'>
-      <img src={props.product.image} alt="" />
-      <h3>{props.product.title}</h3>
-      <p>${props.product.price}</p>
-    </div>
+      <div className='card'>
+        <img src={props.product.image} alt="" height={100} width={100} />
+        <h3>{props.product.title}</h3>
+        <p>$ {props.product.price}</p>
+      </div>
     </>
   )
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,35 +1,30 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
 function ProductList() {
-  const [products,setProducts] = useState([]);
-  const [searchTerm,setSearchTerm] = useState("");
-
+  const [products,SetProducts] = useState([]);
+  const [searchTerm , setSearchTerm] = useState("");
 
   useEffect(()=>{
-    fetch('https://fakestoreapi.com/products').then(res => res.json()).then(rel => setProducts(rel)).catch(err => console.error(err)); 
+    fetch('https://fakestoreapi.com/products').then(res => res.json()).then(json => SetProducts(json)).catch((e) => console.error(e))
+  },[]);
+  const FilteredProducts = products.filter(product => product.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
-  },[])
 
-  const filteredProducts = products.filter(product => product.title.toLowerCase().includes(searchTerm.toLowerCase()));
-  
   return (
-    <div>
-      <input 
-      type="text"
-      name='searchTerm'
-      placeholder='Search Products.....'
-      value={searchTerm}
-      onChange={(e)=> {setSearchTerm(e.target.value)}}
-      style={{padding:10 ,width: '100%', marginBottom:20 }}
-       />
-
-      {filteredProducts.length === 0 ? ( <h1>No products Found</h1>): (
-
-        <div style={{display:'flex' , flexWrap:'wrap' , gap:10}}>
-          {filteredProducts.map(product => (<ProductCard product = {product} key = {product.id}/>))}
-        </div>
-      )}
-    </div>
+    <>
+      <div>
+        <input style={{ width: '93%' , fontWeight: 'bold' ,padding:10 ,height:30 ,marginBottom: 20, borderRadius:7}}
+         type="text"
+         name='search'
+         placeholder='Search for Products....'
+         value={searchTerm}
+         onChange={(e)=> setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div style={{display: 'flex' , flexWrap: 'wrap' , gap:20 }}>
+        {FilteredProducts.map(product => (<ProductCard key = {product.id} product = {product}/>))}
+      </div>
+    </>
   )
 }
 
